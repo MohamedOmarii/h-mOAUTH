2a06e950cae1474e45df9c1e4a91e4f8d2547679
Mohamad Omarii <mohamed.44301@hotmail.com>
2017-04-10 10:08:05 +0300

    welcome page

--- routes/welcome.js
+++ routes/welcome.js
@@ -1,13 +1,42 @@
-const path = require('path');
 
-
-const handler = (request, reply) => {
-  console.log(request.query.code);
-}
+const request = require('request');
+const env = require('env2')('./config.env');
+const url = require('url');
+const queryString = require('querystring');
 
 
 module.exports = {
   method: 'GET',
   path: '/welcome',
-  handler: handler
+  handler: (req, rep) => {
+    var parsedUrl = url.parse(req.url);
+
+    var sendAuth = {
+      'client_id': process.env.CLIENT_ID,
+      'client_secret': process.env.CLIENT_SECRET,
+      'code': parsedUrl.query.code
+    }
+    console.log(sendAuth.code);
+
+    var options = {
+      url: 'https://github.com/login/oauth/access_token',
+      'method': 'POST',
+      'body': queryString.stringify(sendAuth)
+    };
+
+    request(options, function (error, response, body) {
+      if (error) {
+        console.log('Error:', error);
+        return;
+      }
+      var responseBody = queryString.parse(body);
+
+      if (responseBody.access_token) {
+        rep('Access granted');
+      } else {
+        rep('Access denied');
+      }
+      console.log('body:', queryString.parse(body)); // Print the HTML for the Google homepage.
+    });
+  }
 }
